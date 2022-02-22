9fc0aec1fb5fdf00828242b3d94b433cfc0f7673
huangshuai <862997561@qq.com>
2022-02-22 22:51:44 +0800

    '探索mobx6、代码支持装饰器'

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {makeAutoObservable} from 'mobx';
+import {observer} from 'mobx-react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/**
+ * 1.初始化mobx容器仓库
+ * 2.在组建中使用mobx容器状态
+ * 3.在组件中发起action修改容器中的状态
+ */
+
+class Store {
+
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    count = 0
+
+    increment = () => {
+        debugger;
+        this.count++
+    }
+}
+
+@observer
+class App extends React.Component {
+    render() {
+        const {store} = this.props;
+        debugger;
+        return (
+            <div>
+                <p>{store.count}</p>
+                <p>
+                    <button onClick={store.increment}>Increment</button>
+                </p>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App store={new Store()}/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
